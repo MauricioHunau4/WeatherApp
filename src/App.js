13038cf31a73e9e1cb590683f21d7e94d54b18be
Mauricio Hunau <maurih@live.com>
2022-04-25 19:47:31 -0300

    proyecto terminado con cambios originales

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const api = {
   key: "dcf8f75fbb497255bd21327f08920eea",
@@ -9,34 +9,46 @@ const api = {
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-  const [info, setInfo] = useState([]);
+  const [area, setArea] = useState("");
+  const [currencies, setCurrencies] = useState("");
+  const [capital, setCapital] = useState("");
+  const [demonym, setDemonym] = useState("");
+  const [languages, setLanguages] = useState("");
+  const [population, setPopulation] = useState("");
+  const [flag, setFlag] = useState("")
   const [countryCode, setCountryCode] = useState("")
-  
-  
+
+
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-      .then(res => res.json())
-      .then(result => {
-        setWeather(result);
-        setCountryCode(result.sys.country);
-        setQuery('');
-      });
-      }
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setCountryCode(result.sys.country);
+          setQuery('');
+        });
+    }
   }
 
-
-  useEffect(()=>{
-    if(countryCode !== ""){
+  useEffect(() => {
+    if (countryCode !== "") {
       fetchApi();
     }
   }, [countryCode]);
 
-
-  const fetchApi = async() => {
-      const response = await fetch(`https://restcountries.com/v2/alpha/${countryCode}`);
-      const responseJSON = await response.json();
-      setInfo(responseJSON);
+  const fetchApi = async () => {
+    await fetch(`https://restcountries.com/v2/alpha/${countryCode}`)
+      .then(res => res.json())
+      .then(data => {
+        setFlag(data.flag)
+        setArea(data.area)
+        setCapital(data.capital)
+        setCurrencies(data.currencies[0].name)
+        setDemonym(data.demonym)
+        setLanguages(data.languages[0].name)
+        setPopulation(data.population)
+      })
   }
 
 
@@ -51,12 +63,12 @@ function App() {
 
     return `${day} ${date} ${month} ${year}`
   }
-   
+
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 25) ? 'app warm' : 'app') : 'app'}>
-    <main>
+      <main>
         <div className="search-box">
-          <input 
+          <input
             type="text"
             className="search-bar"
             placeholder="Search..."
@@ -66,55 +78,55 @@ function App() {
           />
         </div>
         {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className='box'>
-            <div>
-              <div className="location-box">
-                <div className="location">{weather.name}, {weather.sys.country}</div>
-                <div className="date">{dateBuilder(new Date())}</div>
-              </div>
-              <div className="weather-box">
-                <div className="temp">
-                  {Math.round(weather.main.temp)}°c
+          <div>
+            <div className='box'>
+              <div>
+                <div className="location-box">
+                  <div className="location">{weather.name}, {weather.sys.country}</div>
+                  <div className="date">{dateBuilder(new Date())}</div>
                 </div>
-                <div className="weather">{weather.weather[0].main}</div>
+                <div className="weather-box">
+                  <div className="temp">
+                    {Math.round(weather.main.temp)}°c
+                  </div>
+                  <div className="weather">{weather.weather[0].main}</div>
+                </div>
+              </div>
+              <div>
+                <div className="flags">  <img
+                  src={flag}
+                  width="256"
+                  height="192"
+                  alt={weather.name}></img>
+                </div>
+                
               </div>
             </div>
-            <div>
-             <div className="flags">  <img
-                src={info.flag}
-                width="256"
-                height="192"
-                alt={weather.name}></img>
+              <div className='information'>
+                <div>
+                  <div className='infos'>
+                    Capital: {capital}
+                  </div>
+                  <div className='infos'>
+                    Currencie:  {currencies}
+                  </div>
+                  <div className='infos'>
+                    Population: {population}
+                  </div>
+                </div>
+                <div>
+                  <div className='infos'>
+                    Demonym: {demonym}
+                  </div>
+                  <div className='infos'>
+                    Area: {area} m<sup>2</sup>
+                  </div>
+                  <div className='infos'>
+                    Language: {languages} 
+                  </div>
+                </div>
               </div>
-              
-            </div>
           </div>
-          <div className='information'>
-            <div>
-              <div className='infos'>
-                Capital: {info.capital}
-              </div> 
-              <div className='infos'>
-                Region:  {info.region}
-              </div>
-                <div className='infos'>
-                Population: {info.population}
-              </div>
-            </div>  
-            <div>
-              <div className='infos'>
-                Demonym: {info.demonym}
-              </div>
-              <div className='infos'>
-               Subregion: {info.subregion}
-              </div>
-              <div className='infos'> 
-                Area: {info.area} m<sup>2</sup>
-              </div>   
-            </div>
-          </div> 
-        </div>
         ) : ('')}
       </main>
     </div>
